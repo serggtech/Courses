cc8da855434d80a5fc083fae3ec1bc367ebaa9f5
serggtech-home <serggtech@gmail.com>
2024-02-22 01:10:01 +0300

    !

--- Innodom/hw/10/main.py
+++ Innodom/hw/10/main.py
@@ -12,10 +12,12 @@ count_survived_child = 0
 count_male_all = 0
 count_male_child = 0
 count_male = 0
+count_male_no_age = 0
 
 count_female_all = 0
 count_female_child = 0
 count_female = 0
+count_female_no_age = 0
 
 count_child = 0
 
@@ -27,14 +29,17 @@ with open("titanic.csv", "r", newline="") as csv_file:
 
         if row["Survived"] == "1":
             count_survived += 1
-        else:
+        elif row["Survived"] == "0":
             count_no_survived += 1
 
-        count_male_all
+        if row["Sex"] == "male":
+            count_male_all += 1
         if row["Sex"] == "male" and row["Age"] >= "15":
             count_male += 1
         if row["Sex"] == "male" and row["Age"] <= "15":
             count_male_child += 1
+        if row["Sex"] == "male" and row["Age"] == "":
+            count_male_no_age += 1
 
         if row["Sex"] == "female" and row["Age"] >= "15":
             count_female += 1
@@ -56,3 +61,12 @@ print(count_male + count_male_child + count_female + count_female_child)
 print(type(read_data))
 print()
 print(read_data)
+with open("titanic_out.txt", "w") as csv_file:
+    csv_file.write(
+        f"Количество выживших: {count_survived} , "
+        f"Количество не выживших: {count_no_survived} ,"
+        f"Количество мужчин: {count_male_all} , "
+        f"Количество мужчин no_age: {count_male_no_age} ,"
+        f"Количество мужчин <= 15: {count_male_child}"
+    )
+    # write_data = csv.DictWriter
